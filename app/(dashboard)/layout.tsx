925bd60243c69614b51d0d065743a6e47ff4a6eb
Berto Mill <bertomill@Bertos-MacBook-Pro.local>
2024-11-08 11:08:51 -0500

    Initial commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,10 @@
+const DashboardLayout = ({ children }) => {
+    return (
+        <>
+            <h1>Dashboard</h1>
+            {children}
+        </>
+    );
+}
+
+export default DashboardLayout
---
+++ components/NewTodoForm.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { newTodo } from '@/utils/actions'
+
+const NewTodoForm = () => {
+    return (
+      <div>
+        <form action={newTodo}>
+          <input type="text" name="todo" className="border border-black" />
+          <button type="submit">create</button>
+        </form>
+      </div>
+    )
+  }
+  
+  export default NewTodoForm
